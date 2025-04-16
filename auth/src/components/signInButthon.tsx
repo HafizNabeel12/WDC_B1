83e98f497b8cb2f59cf3fbb2bbe81872f9e02ff6
HafizNabeel12 <138764369+HafizNabeel12@users.noreply.github.com>
2025-04-16 06:44:22 +0500

    Authentication

---
+++ auth/src/components/signInButthon.tsx
@@ -0,0 +1,12 @@
+// import { SignInButton } from "@clerk/nextjs"
+// const signInButthon = () => {
+//   return (
+//     <div>
+//     <div className="h-3 w-3 rounded bg-green-500">
+//         <SignInButton />
+//     </div>
+//     </div>
+//   )
+// }
+
+// export default signInButthon
